aac0ee75681d588c829d04a8fc50d5ca9f711f9a
Jessica Höck <thedirtypanda001@gmail.com>
2020-05-26 16:16:45 +0200

    Edited update 427 to the new view.

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -3088,7 +3088,7 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
             $oTransaction = $oDB->beginTransaction();
 
             // Menu Link needs to be updated, cause we will revert the filemanager and enable the older one.
-            $oDB->createCommand()->update('{{surveymenu_entries}}', array('menu_link' => 'admin/survey/sa/rendersidemenulink&subaction=resources'), "name='resources'");
+            $oDB->createCommand()->update('{{surveymenu_entries}}', array('menu_link' => '', 'action' => 'updatesurveylocalsettings', 'template' => 'editLocalSettings_main_view', 'partial' => '/admin/survey/subview/accordion/_resources_panel'), "name='resources'");
             $oTransaction->commit();
         }
     } catch (Exception $e) {
