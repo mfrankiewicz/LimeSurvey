858267199d3ff3473c22d494c0a2129b299df39b
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2020-05-29 12:30:37 +0200

    Dev: Minor formating

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -3088,7 +3088,16 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
             $oTransaction = $oDB->beginTransaction();
 
             // Menu Link needs to be updated, cause we will revert the filemanager and enable the older one.
-            $oDB->createCommand()->update('{{surveymenu_entries}}', array('menu_link' => '', 'action' => 'updatesurveylocalsettings', 'template' => 'editLocalSettings_main_view', 'partial' => '/admin/survey/subview/accordion/_resources_panel'), "name='resources'");
+            $oDB->createCommand()->update(
+                '{{surveymenu_entries}}',
+                array(
+                    'menu_link' => '',
+                    'action'    => 'updatesurveylocalsettings',
+                    'template'  => 'editLocalSettings_main_view',
+                    'partial'   => '/admin/survey/subview/accordion/_resources_panel'
+                ),
+                "name='resources'"
+            );
             $oDB->createCommand()->update('{{settings_global}}', array('stg_value' => 427), "stg_name='DBVersion'");
             $oTransaction->commit();
         }
